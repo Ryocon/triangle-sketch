facde55fb0c9ee57f182f98cf2a323ab2a550832
roconn <roconn25@gmail.com>
2023-01-17 15:51:24 +0000

    2 examples of rectangle drawings

--- sketch.js
+++ sketch.js
@@ -1,13 +1,44 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [ 1080, 1080 ]
 };
 
 const sketch = () => {
+
+  let x, y, w, h;
+
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+
+    x = width * 0.5
+    y = height * 0.5
+    w = width * 0.6
+    h = height * 0.1
+
+    // saves and resets(?)
+    context.save()
+    // anchors the rectangle in the centre
+    context.translate(x, y)
+
+    context.strokeStyle = 'blue'
+
+    // a way of drawing the rectangle
+    // context.strokeRect(w * -0.5, h * 0.5, w, h)
+
+    // rectangle drawn point by point
+    // easier to move and control
+    context.beginPath()
+    context.moveTo(w * -0.5, h * -0.5)
+    context.lineTo(w * 0.5, h * -0.5)
+    context.lineTo(w * 0.5, h * 0.5)
+    context.lineTo(w * -0.5, h * 0.5)
+    context.closePath()
+    context.stroke()
+
+    context.restore()
+
   };
 };
 
