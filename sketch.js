d8f15889646dc0f037f0c895edd5ff2519f84948
roconn <roconn25@gmail.com>
2023-02-26 14:22:56 +0000

    added random + control array

--- sketch.js
+++ sketch.js
@@ -4,26 +4,39 @@ const math = require('canvas-sketch-util/math')
 const random = require('canvas-sketch-util/random')
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [ 1080, 1080 ],
+  // animate: true
 };
 
-const sketch = () => {
+const sketch = ({ context, width, height }) => {
 
   let x, y, w, h;
 
   const num = 20
   const degrees = -30
 
-  return ({ context, width, height }) => {
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
+  // being used to control each random generation of the frame
+  // stops the random from happening constantly
+  const rects = []
 
-    for (let i = 0; i < num; i++ ) {
+  for (let i = 0; i < num; i++ ) {
 
     x = random.range(0, width)
     y = random.range(0, height)
     w = random.range(200, 600)
     h = random.range(40, 200)
+
+    rects.push({ x, y, w, h })
+  }
+
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+
+    rects.forEach(rect => {
+
+    // destructed rect
+    const { x, y, w, h, } = rect
 
     // saves and resets(?)
     context.save()
@@ -37,8 +50,7 @@ const sketch = () => {
     context.stroke()
  
     context.restore()
-    }
-
+    })
   };
 };
 
